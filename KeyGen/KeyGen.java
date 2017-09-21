104c9044dfddf51717ba1ff4156e4705a6ab4779
tgolnoft <tgolnoft@gmail.com>
2017-09-21 13:32:47 -0600

    Initial Commmit

---
+++ KeyGen/KeyGen.java
@@ -0,0 +1,86 @@
+import java.security.*;
+import java.io.*;
+import java.security.spec.*;
+import java.util.*;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class KeyGen{
+  
+  private final static SecureRandom random = new SecureRandom();
+  
+  //Public and Private Keys - X for sender, Y for Receiver
+  private final static String X_PUBLIC_KEY_FILE = "XPublic.key";
+  private final static String Y_PUBLIC_KEY_FILE = "YPublic.key";
+  
+  private final static String X_PRIVATE_KEY_FILE = "XPrivate.key";
+  private final static String Y_PRIVATE_KEY_FILE = "YPrivate.key";
+  
+  private final static String SYMMETRIC_KEY_FILE= "symmetric.key";
+  
+  
+  public static void main (String[] args) throws Exception{
+    generateKeys();
+  }
+
+  private static void generateKeys() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException{
+    //Generate a pair of keys
+    SecureRandom random = new SecureRandom();
+    KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+    generator.initialize(1024, random);  //1024: key size in bits
+    KeyPair pair = generator.generateKeyPair();
+    Key pubKey = pair.getPublic();
+    Key privKey = pair.getPrivate();
+
+    //get the parameters of the keys: modulus and exponet
+    KeyFactory factory = KeyFactory.getInstance("RSA");
+    RSAPublicKeySpec pubKSpec = factory.getKeySpec(pubKey, 
+        RSAPublicKeySpec.class);
+    RSAPrivateKeySpec privKSpec = factory.getKeySpec(privKey, 
+        RSAPrivateKeySpec.class);
+
+    //save the parameters of the keys to the files
+    saveToFile(X_PUBLIC_KEY_FILE, pubKSpec.getModulus(), 
+        pubKSpec.getPublicExponent());
+    saveToFile(X_PRIVATE_KEY_FILE, privKSpec.getModulus(), 
+        privKSpec.getPrivateExponent());
+  
+    //Do the same as above for the next pair of keys
+    pair = generator.generateKeyPair();
+    pubKey = pair.getPublic();
+    privKey = pair.getPrivate();
+
+    pubKSpec = factory.getKeySpec(pubKey, 
+        RSAPublicKeySpec.class);
+    privKSpec = factory.getKeySpec(privKey, 
+        RSAPrivateKeySpec.class);
+
+    saveToFile(Y_PUBLIC_KEY_FILE, pubKSpec.getModulus(), 
+        pubKSpec.getPublicExponent());
+    saveToFile(Y_PRIVATE_KEY_FILE, privKSpec.getModulus(), 
+        privKSpec.getPrivateExponent());
+
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Enter the 16 character string for the symmetric key: ");
+    byte[] sym = scan.nextLine().getBytes("UTF-8");
+    Files.write(Paths.get(SYMMETRIC_KEY_FILE), sym);
+  }
+
+  //save the prameters of the public and private keys to file
+  public static void saveToFile(String fileName,
+    BigInteger mod, BigInteger exp) throws IOException {
+
+    ObjectOutputStream oout = new ObjectOutputStream(
+      new BufferedOutputStream(new FileOutputStream(fileName)));
+
+    try {
+      oout.writeObject(mod);
+      oout.writeObject(exp);
+    } catch (Exception e) {
+      throw new IOException("Unexpected error", e);
+    } finally {
+      oout.close();
+    }
+  }
+}
---
+++ Receiver/Readers.java
@@ -0,0 +1,84 @@
+import java.security.*;
+import java.util.*;
+import java.security.spec.*;
+import java.io.*;
+import java.math.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Readers{
+  private static final String X_PUB_FILE_NAME = "XPublic.key";
+  private static final String X_PRIV_FILE_NAME = "XPrivate.key";
+  private static final String Y_PUB_FILE_NAME = "YPublic.key";
+  private static final String Y_PRIV_FILE_NAME = "YPrivate.key";
+  private static final String SYM_KEY_FILE_NAME = "symmetric.key";
+  public static void main (String[] args)throws Exception {
+    PublicKey pubKeyX = readPubKeyFromFile(X_PUB_FILE_NAME);
+    PrivateKey privKeyX = readPrivKeyFromFile(X_PRIV_FILE_NAME);
+    
+    PublicKey pubKeyY = readPubKeyFromFile(Y_PUB_FILE_NAME);
+    PrivateKey privKeyY = readPrivKeyFromFile(Y_PRIV_FILE_NAME);
+    byte[] symkey = readSymKeyFromFile(SYM_KEY_FILE_NAME);
+    System.out.println("Symmetric Key(decimal): " + Arrays.toString(symkey));
+  }
+
+  //read key parameters from a file and generate the public key 
+  public static PublicKey readPubKeyFromFile(String keyFileName) 
+      throws IOException {
+    InputStream in = 
+        new FileInputStream(keyFileName);
+    ObjectInputStream oin =
+        new ObjectInputStream(new BufferedInputStream(in));
+
+    try {
+      BigInteger m = (BigInteger) oin.readObject();
+      BigInteger e = (BigInteger) oin.readObject();
+
+      System.out.println("Readers from " + keyFileName + "\nmodulus = " + 
+          m.toString() + "\nexponent = " + e.toString() + "\n");
+
+      RSAPublicKeySpec keySpec = new RSAPublicKeySpec(m, e);
+      KeyFactory factory = KeyFactory.getInstance("RSA");
+      PublicKey key = factory.generatePublic(keySpec);
+
+      return key;
+    } catch (Exception e) {
+      throw new RuntimeException("Spurious serialisation error", e);
+    } finally {
+      oin.close();
+    }
+  }
+
+
+  //read key parameters from a file and generate the private key 
+  public static PrivateKey readPrivKeyFromFile(String keyFileName) 
+      throws IOException {
+
+    InputStream in = 
+        new FileInputStream(keyFileName);
+    ObjectInputStream oin =
+        new ObjectInputStream(new BufferedInputStream(in));
+
+    try {
+      BigInteger m = (BigInteger) oin.readObject();
+      BigInteger e = (BigInteger) oin.readObject();
+
+      System.out.println("Read from " + keyFileName + "\nmodulus = " + 
+          m.toString() + "\nexponent = " + e.toString() + "\n");
+
+      RSAPrivateKeySpec keySpec = new RSAPrivateKeySpec(m, e);
+      KeyFactory factory = KeyFactory.getInstance("RSA");
+      PrivateKey key = factory.generatePrivate(keySpec);
+
+      return key;
+    } catch (Exception e) {
+      throw new RuntimeException("Spurious serialisation error", e);
+    } finally {
+      oin.close();
+    }
+  }
+  public static byte[] readSymKeyFromFile(String keyFileName) throws Exception{
+    return Files.readAllBytes(Paths.get(keyFileName));   
+  }
+}
